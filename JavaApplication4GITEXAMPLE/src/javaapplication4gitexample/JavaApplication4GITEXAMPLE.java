ca3bdbc5f0552bbe4cc55c505aed84efa5bbe8ba
Sergio De Freitas <sergiodfdf@gmail.com>
2012-05-27 18:53:35 -0400

    Prueba numero 1

---
+++ JavaApplication4GITEXAMPLE/src/javaapplication4gitexample/JavaApplication4GITEXAMPLE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4gitexample;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication4GITEXAMPLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("aaaaa");
+        // TODO code application logic here
+    }
+}
