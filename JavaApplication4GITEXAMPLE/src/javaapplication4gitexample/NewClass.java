8b55860b382118dd04058da39a4213fce4833447
Sergio De Freitas <sergiodfdf@gmail.com>
2012-05-27 19:04:40 -0400

    Prueba numero 4

---
+++ JavaApplication4GITEXAMPLE/src/javaapplication4gitexample/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4gitexample;
+
+/**
+ *
+ * @author User
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        
+        System.out.println("aaaaaaaaaaa!!");
+        // TODO code application logic here
+    }
+}
